cce8a0187dd10183003896aa7c652a88e98c1f04
abhinavsalesforce01 <abhinav.tyagi@salesforce.com>
2024-07-31 17:47:08 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,6 +3,6 @@ var app = express();
 app.get('/', function (req, res) {
   res.send('Hello World!');
 });
-app.listen(process.env.PORT || 4000, function () {
+app.listen(process.env.PORT || 5000, function () {
   console.log('Example app listening on port 3000!');
 });
